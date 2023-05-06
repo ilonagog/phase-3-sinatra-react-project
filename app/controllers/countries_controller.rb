9ee0979ea35bb374f0cb3b0c04496bc740b79894
ilona <gogiashvili.ilona@gmail.com>
2023-05-05 23:28:54 -0400

    added get in controllers

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,6 +2,11 @@ class CountriesController < ApplicationController
 
     get '/countries' do
         countries = Country.all
-        countries.to_json
+        countries.to_json(include: :tours)
       end
+
+      get '/countries/:id' do
+        country = Country.find_by(id: params[:id])
+        country.to_json(include: :tours)
+    end
 end
